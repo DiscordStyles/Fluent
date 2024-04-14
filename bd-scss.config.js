f7c8e61425c7893c827ee4c12fd07e63cf924cfd
TomyGamy <95078425+TomyGamy@users.noreply.github.com>
2024-04-14 02:24:49 +0200

    bring back the thing (why did I delet it?)

---
+++ bd-scss.config.js
@@ -0,0 +1,14 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Fluent',
+		author: 'Gibbu',
+		version: '1.0.10',
+		description: 'Brings the look of Windows 11 to Discord.',
+		source: 'https://github.com/DiscordStyles/Fluent',
+		invite: 'ZHthyCw',
+		authorId: '174868361040232448'
+	},
+	baseImport: 'https://discordstyles.github.io/Fluent/Fluent.css',
+	addons: [['src/addons/_icons.scss', 'dist/Icons.css']]
+};
